da1c990ce76a5de8ddd13a175d46424f86e36e2d
RuthA120 <rassefa@smu.edu>
2025-06-09 21:23:29 -0500

    Update Register Page

--- frontend/src/pages/RegisterPage.jsx
+++ frontend/src/pages/RegisterPage.jsx
@@ -10,8 +10,19 @@ export default function RegisterPage() {
       </div>
       <div className="form-container">
         <div className='form-box'>
-          <h2>Create an Account</h2>
-          <line></line>
+          <h2 className="header">Create an Account</h2>
+
+          <h3 class ="username-header">Username</h3>
+          <input type="text" className="username-input" placeholder="Enter your username" />
+
+          <h3 class ="email-header">Email</h3>
+          <input type="text" className="email-input" placeholder="Enter your email" />
+
+          <h3 class ="password-header">Password</h3>
+          <input type="text" className="password-input" placeholder="Enter your password" />
+        
+          <button id="register">Sign Up</button>
+          <a href="/login" className="login-link">Login to existing account</a>
         </div>
       </div>
     </div>
